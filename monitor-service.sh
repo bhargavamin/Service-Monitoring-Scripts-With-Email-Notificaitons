3aa02a4d892ae19b0895d5cc7f76a1431816af43
Bhargav Amin <aminbhargav007@gmail.com>
2016-05-08 23:10:06 +0530

    Update monitor-service.sh

--- monitor-service.sh
+++ monitor-service.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 sudo yum install mailx
+sudo ln -s /bin/mailx /bin/email
+sudo touch /opt/monitor-service.sh
+sudo chmod 777 /opt/monitor-service.sh
 
-
+# Copy paste below script in monitor-service.sh
 CHECK=$0
-SERVICE="nginx"
-email=<emailid>
+SERVICE="<enter service name here>"
+email=<email_id_here>
 DATE=`date`
 OUTPUT=$(ps aux | grep -v grep | grep -v $CHECK |grep $SERVICE)
 echo $OUTPUT
@@ -18,3 +21,17 @@ subject="URGENT: $SERVICE ON RUBY APP SERVER HAS STOPPED, WILL START IT AGAIN!"
 echo "$SERVICE at ruby app server wasn't running and has been started" | mail -s "$subject" $email
 sudo /etc/init.d/$SERVICE start
 fi
+
+#Run the script manually
+
+sudo bash monitor-service.sh
+
+# OR run script through crontab
+
+#Copy paste following line in /etc/crontab
+
+* * * * * root bash /opt/monitor-service.sh
+
+#this will run script every min to check if service is running or not, if not then it send a mail to specified recipient
+
+
